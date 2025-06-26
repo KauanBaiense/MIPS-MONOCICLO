db98cc0ed530328620d529fd8807fe08c7e948df
KauanBaiense <97204720+KauanBaiense@users.noreply.github.com>
2025-06-26 18:29:40 -0300

    Update main.vhdl

--- main.vhdl
+++ main.vhdl
@@ -15,29 +15,45 @@ entity main is
     );
 end main;
 architecture Behavior of controle is
-    signal sPC,M5,S1,D2,ES,Aumenta : unsigned(31 downto 0);
+    signal sPC,M5,S1,D2,ES : unsigned(31 downto 0);
     signal inst : unsigned(25 downto 0);
     signal D1 : unsigned(27 downto 0);
+    signal D : unsigned(14 downto 0);
+    signal A,B,C : unsigned(4 downto 0);
     signal FontePC : std_logic;
 begin
 
     PC: entity work.unsigned_register(behavior)
         generic map(N => 32)
         port map(clk => clk, enable => signals, d => M5, q => sPC);
-    
+    -- PARTE DE BAIXO MIPS
+    -- sMem : entity work.memory
     inst <= sMem(25 downto 0);
     
-    Aumenta <= inst(15 downto 0);
-    ES <= resize(aumenta,32); -- sinal de extensão
-    S1 <= '4' + sPC; --saida somador1
-    D1 <= inst & "00"; 
+    D <= inst(14 downto 0);
     
-    D2 <= S1(31 downto 28) & D1;
+    ES <= resize(D,32); -- extensão de sinal
+    A,B,C
+
+
+    -- PARTE DE CIMA MIPS
+    S1 <= '4' + sPC; -- somador 1
+    D1 <= inst & "00"; -- Deslocamento de 2 bits para a esquerda *4
+    D2 <= S1(31 downto 28) & D1; -- concatenação D1 E (PC + 4) 4 bits
+    
+    MUX5: entity work.mux_2to1(behavior)
+        generic map(N => 32)
+        port map(sel => /*VEMDOCONTROLE*/ DVI, in_0 => M4, in_1 => D2, y => M5);
+    
+    MUX4: entity work.mux_2to1(behavior)
+        generic map(N => 32)
+        port map(sel => FontePC, in_0 => S1, in_1 => S2, y => M4);
     
     FontePC <= zero and DvC;
-    Mux: entity work.mux_2to1(behavior)
+    ESv4 <= ES(29 downto 0) & "00";
+    somador2: entity work.somador(behavior)
         generic map(N => 32)
-        port map(sel => FontePC, in_0 => S1, in_1 => S2, y => M5);
+        port map(a => S1, b => ESv4, y => S2);
     
     
 

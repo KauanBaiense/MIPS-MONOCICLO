af69e5f6af5e6b7612e3b4e95d8666050a34b447
viniciusmaga0202 <viniciusmagaturcatto@gmail.com>
2025-06-27 11:38:34 -0300

    Update Banco_Reg.vhdl

--- Banco_Reg.vhdl
+++ Banco_Reg.vhdl
@@ -1,1 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
+entity REGBANK is
+    Port (
+        entrada16 : in  STD_LOGIC_VECTOR(15 downto 0);
+        saida32   : out STD_LOGIC_VECTOR(31 downto 0)
+    );
+end entity;
+
+architecture arch of REGBANK is
+begin
+
+    saida32 <= (others => entrada16(15)) & entrada16;
+end architecture;

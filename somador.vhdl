e050342431625e0bfb5fc001f201e83336eae041
viniciusmaga0202 <viniciusmagaturcatto@gmail.com>
2025-06-26 23:19:29 -0300

    Update somador32.vhdl

---
+++ somador.vhdl
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity somador32 is
+    Port (
+        A, B : in  STD_LOGIC_VECTOR(31 downto 0);
+        S    : out STD_LOGIC_VECTOR(31 downto 0)
+    );
+end entity;
+architecture logic of somador32 is
+begin
+    S <= std_logic_vector(unsigned(A) + unsigned(B));
+end architecture;

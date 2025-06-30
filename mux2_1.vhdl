7681484b9d26ea9d1912db15d1264eb11adaca48
GIUSEPPE ZILIO <123280477+GiuseppeZilio@users.noreply.github.com>
2025-06-30 02:29:36 -0300

    mux2_1.vhdl
    mux2_1 n bits

--- mux2_1.vhdl
+++ mux2_1.vhdl
@@ -1,10 +1,11 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 entity mux2_1 is
+    generic (n : integer := 8);
     Port (
         sel : in  STD_LOGIC;
-        A, B : in  STD_LOGIC_VECTOR(31 downto 0);
-        Y    : out STD_LOGIC_VECTOR(31 downto 0)
+        A, B : in  STD_LOGIC_VECTOR(n-1 downto 0);
+        Y    : out STD_LOGIC_VECTOR(n-1 downto 0)
     );
 end entity;
 
